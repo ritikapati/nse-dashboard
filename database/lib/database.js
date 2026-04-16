6df345ad1283beea3e782381d433fa0eaa98a116
Ritikapati1 <ritikapatil112000@gmail.com>
2026-04-16 12:43:08 +0530

    removing dotenv

--- database/lib/database.js
+++ database/lib/database.js
@@ -1,6 +1,6 @@
-const path = require('path');
+// const path = require('path');
 // require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') });
-require('dotenv').config();
+// require('dotenv').config();
 const { Pool } = require('pg');
 
 const databaseUrl = process.env.DATABASE_URL || [
